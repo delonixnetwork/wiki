429cf19251315bab7331edff86229f657bc26483
mw_dev <79410241+the-mw-dev@users.noreply.github.com>
2025-03-29 00:36:19 +0300

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -21,76 +21,15 @@ const sidebars = {
   },
   {
     type: 'category',
-    label: 'Панель',
+    label: 'Виртуальные сервера',
     items: [
       {
         type: 'doc',
-        id: 'panel/login',
+        id: 'vm/faq',
       },
-      {
-        type: 'doc',
-        id: 'panel/topup',
-      },
-      {
-        type: 'doc',
-        id: 'panel/order',
-      },
-      {
-        type: 'category',
-        label: 'Управление услугой',
-        items: [
-          {
-            type: 'doc',
-            id: 'panel/service/info',
-          },
-          {
-            type: 'doc',
-            id: 'panel/service/backups',
-          },
-          {
-            type: 'doc',
-            id: 'panel/service/tariff',
-          },
-          {
-            type: 'doc',
-            id: 'panel/service/subdomains',
-          },
-          {
-            type: 'doc',
-            id: 'panel/service/reinstall',
-          },
-      
     ],
   },
   ],
-},
-{
-  type: 'category',
-  label: 'Туториалы',
-  items: [
-    {
-      type: 'doc',
-      id: 'service-tutorials/ssh',
-    },
-    {
-      type: 'doc',
-      id: 'service-tutorials/rescue',
-    },
-    {
-      type: 'doc',
-      id: 'service-tutorials/fsresize',
-    },
-    {
-      type: 'doc',
-      id: 'service-tutorials/pterodactyl',
-    },
-    {
-      type: 'doc',
-      id: 'service-tutorials/pteronode',
-    },
-  ]
-}
-  ]
 };
 
 export default sidebars;
