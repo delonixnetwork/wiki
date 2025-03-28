1a993af556422f45403198dffabc2ef27e420541
mw_dev <79410241+the-mw-dev@users.noreply.github.com>
2025-03-29 00:33:56 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,20 +8,20 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Документация IP6.lat',
-  tagline: 'Расскажем и покажем как сделать на IP6.lat',
-  favicon: 'https://pimik.xyz/ip6.lat.png',
+  title: 'База знаний Delonix',
+  tagline: 'Документация по сервису Delonix',
+  favicon: 'https://delonix.pro/static/delonix.png',
 
   // Set the production url of your site here
-  url: 'https://docs.ip6.lat',
+  url: 'https://wiki.delonix.pro',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ip6lat', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  organizationName: 'delonixnetwork', // Usually your GitHub org/user name.
+  projectName: 'wiki', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ip6lat/docs/edit/main/',
+            'https://github.com/delonixnetwork/wiki/edit/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -62,22 +62,22 @@ colorMode: {
       respectPrefersColorScheme: false, // Использовать системные настройки пользователя
     },
       // Replace with your project's social card
-      image: 'https://pimik.xyz/ip6.lat.png',
+      image: 'https://delonix.pro/static/delonix.png',
       navbar: {
-        title: 'Документация IP6.lat',
+        title: 'База знаний Delonix',
         logo: {
-          alt: 'IP6.lat',
-          src: 'https://pimik.xyz/ip6.lat.png',
+          alt: 'Delonix',
+          src: 'https://delonix.pro/static/delonix.png',
         },
         items: [
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Документация',
+            label: 'База знаний',
           },
           {
-            href: 'https://github.com/ip6lat/docs',
+            href: 'https://github.com/delonixnetwork/wiki',
             label: 'GitHub',
             position: 'right',
           },
